bac5f5c39094bb60e80e699d1a751809940a7e33
stmonty <stmontydev@gmail.com>
2026-02-06 15:55:03 -0800

    Switch to lean build file instead of toml

---
+++ c/stub.c
@@ -0,0 +1,1 @@
+void ria_c_stub(void) {}
---
+++ lakefile.lean
@@ -0,0 +1,47 @@
+import Lake
+open System Lake DSL
+
+def openBlasLinkArgs : Array String := Id.run do
+  if Platform.isOSX then
+    #[
+      "-L/opt/homebrew/opt/openblas/lib",
+      "-L/usr/local/opt/openblas/lib",
+      "-lopenblas"
+    ]
+  else if Platform.isWindows then
+    #[
+      "-LC:/msys64/mingw64/lib",
+      "-lopenblas"
+    ]
+  else
+    #[
+      "-L/usr/lib/x86_64-linux-gnu",
+      "-L/usr/local/lib",
+      "-L/usr/lib",
+      "-lopenblas"
+    ]
+
+package ria where
+  moreLinkArgs := openBlasLinkArgs
+
+extern_lib riac pkg := do
+  let cDir := pkg.dir / "c"
+  let cFilesJob ← inputDir cDir (text := true) (filter := fun path => path.extension == some "c")
+  cFilesJob.mapM fun cFiles => do
+    let mut oFiles := #[]
+    for cFile in cFiles do
+      let stem := cFile.fileStem.getD (cFile.fileName.getD "csrc")
+      let oFile := pkg.buildDir / "c" / s!"{stem}.o"
+      let cArgs := #["-I", (← getLeanIncludeDir).toString]
+      compileO oFile cFile cArgs "cc"
+      oFiles := oFiles.push oFile
+    let libPath := pkg.staticLibDir / nameToStaticLib "riac"
+    compileStaticLib libPath oFiles
+    pure libPath
+
+@[default_target]
+lean_lib Ria where
+  needs := #[riac]
+
+lean_exe ria where
+  root := `Main
